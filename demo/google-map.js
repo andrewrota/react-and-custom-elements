30de2e6021a229965c338d58c917d167544a85fc
andrewrota <rota.andrew@gmail.com>
2014-12-31 13:50:10 +0000

    Add button and rename to paper.html

--- demo/google-map.js
+++ demo/google-map.js
@@ -3,10 +3,11 @@
         var React = window.React;
         var GoogleMap = React.createClass({
             render: function() {
-                return React.createElement('google-map', {latitude: '41.8369', longitude: '-87.6847'}, '', this.props.name);
+                return React.createElement('google-map', {latitude: '41.8369', longitude: '-87.6847', ref: 'map'}, '', this.props.name);
             }
         });
         React.render( React.createElement(GoogleMap, null, ' '),
           document.querySelector('body')
         );
+        GoogleMap
 }(window));
--- demo/paper.js
+++ demo/paper.js
@@ -3,7 +3,7 @@
         var React = window.React;
         var PaperToast = React.createClass({
             render: function() {
-                return React.createElement('paper-toast', {text: 'foo', opened: true}, '', this.props.name);
+                return React.createElement('div', null, React.createElement('paper-button', null, 'Hello world', this.props.name), React.createElement('paper-toast', {text: 'foo', opened: true}, '', this.props.name));
             }
         });
         React.render( React.createElement(PaperToast, null, ' '),
